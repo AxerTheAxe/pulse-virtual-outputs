7b2c72ad65c6b096550d5258a4456c2d4b39997a
AxerTheAxe <99703210+AxerTheAxe@users.noreply.github.com>
2023-07-24 16:56:58 -0600

    Added the ability to refresh existing outputs.
    If existing virtual outputs are found during creation, they can now be reloaded instead of just being skipped.

--- pw-virtual-output-routing.sh
+++ pw-virtual-output-routing.sh
@@ -3,7 +3,7 @@
 #--IMPORTANT--- Change this to some text unique to your audio device.
 # You can run 'pactl list short sinks' then pick a descriptive part of the name of your audio output device, such as the name.
 # For example, if my desired output is "alsa_output.usb-Razer_Razer_Kraken_Ultimate_00000000-00.analog-stereo", then I can use "Razer_Kraken_Ultimate" for the sink descriptor.
-sink_descriptor=""
+sink_descriptor="Razer_Kraken_Ultimate"
 
 # Wait until the 'pactl' command is ready for use and fail if it takes too long
 wait_for_pactl() {
@@ -20,6 +20,12 @@ wait_for_pactl() {
     done
 }
 
+# Get index of the specified loaded module
+get_module_index() {
+    local name=$1
+    pactl list short modules | awk -v name="$name" '$3 ~ name {print $1}'
+}
+
 # Create new sinks
 create_virtual_output() {
     local name=$1
@@ -27,7 +33,12 @@ create_virtual_output() {
     if ! pactl list short sinks | grep $name; then
         pactl load-module module-combine-sink sink_name=$name slaves=$sink_name
     else
-        echo -e "\e[33mWarning, output with name '$name' already exists. Skipping.\e[0m"
+	module_index=$(get_module_index "$name")
+
+        echo -e "\e[33mWarning, reloading '$name' because an output with that name was already found.\e[0m"
+
+	pactl unload-module "$module_index"
+        pactl load-module module-combine-sink sink_name=$name slaves=$sink_name
     fi
 }
 
